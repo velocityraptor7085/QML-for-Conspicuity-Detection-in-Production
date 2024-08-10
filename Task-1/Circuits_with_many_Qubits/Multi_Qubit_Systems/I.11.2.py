a579a5acbc17605c6f5c8560819b6cddb1f98966
Archit Chadalawada <drarchitch@gmail.com>
2024-08-10 07:44:21 +0530

    Add files via upload

---
+++ Task-1/Circuits_with_many_Qubits/Multi_Qubit_Systems/I.11.2.py
@@ -0,0 +1,20 @@
+# Creates a device with *two* qubits
+dev = qml.device("default.qubit", wires=2)
+
+
+@qml.qnode(dev)
+def two_qubit_circuit():
+    ##################
+    # YOUR CODE HERE #
+    ##################
+
+    # PREPARE |+>|1>
+
+    # RETURN TWO EXPECTATION VALUES, Y ON FIRST QUBIT, Z ON SECOND QUBIT
+    qml.Hadamard(wires=0)
+    qml.PauliX(wires=1)
+    
+    return (qml.expval(qml.PauliY(0)), qml.expval(qml.PauliZ(1)))
+
+
+print(two_qubit_circuit())
---
+++ Task-1/Circuits_with_many_Qubits/Multi_Qubit_Systems/I.11.3.py
@@ -0,0 +1,18 @@
+dev = qml.device("default.qubit", wires=2)
+
+
+@qml.qnode(dev)
+def create_one_minus():
+    ##################
+    # YOUR CODE HERE #
+    ##################
+
+    # PREPARE |1>|->:
+    qml.PauliX(wires=0)
+    qml.PauliX(wires=1)
+    qml.Hadamard(wires=1)
+
+    # RETURN A SINGLE EXPECTATION VALUE Z \otimes X:
+    return qml.expval(qml.PauliZ(wires=0) @ qml.PauliX(wires=1))
+
+print(create_one_minus())
