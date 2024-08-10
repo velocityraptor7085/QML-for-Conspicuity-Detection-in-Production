15f402e8cb7498679d76cc7aeb99a24108491681
Archit Chadalawada <drarchitch@gmail.com>
2024-08-10 07:39:47 +0530

    Add files via upload

---
+++ Task-1/Single_Qubit_Gates/Universal_Gate_Sets/I.7.2.py
@@ -0,0 +1,23 @@
+dev = qml.device("default.qubit", wires=1)
+
+
+@qml.qnode(dev)
+def convert_to_rz_rx():
+    ##################
+    # YOUR CODE HERE #
+    ##################
+
+    # IMPLEMENT THE CIRCUIT IN THE PICTURE USING ONLY RZ AND RX:
+    #Hadamard
+    qml.RZ(np.pi/2, wires=0)
+    qml.RX(np.pi/2, wires=0)
+    qml.RZ(np.pi/2, wires=0)
+    #S
+    qml.RZ(np.pi/2, wires=0)
+    #T*
+    qml.RZ(-np.pi/4, wires=0)
+    #Y
+    qml.RX(np.pi, wires=0)
+    qml.RZ(np.pi, wires=0)
+
+    return qml.state()
