4fc1cf9f5fc898b85a464e10a5000d0914c30fb3
Archit Chadalawada <drarchitch@gmail.com>
2024-08-10 07:40:22 +0530

    Create I.10.1.py

---
+++ Task-1/Single_Qubit_Gates/What_Did_You_Expect/I.10.1.py
@@ -0,0 +1,18 @@
+dev = qml.device("default.qubit", wires=1)
+
+
+@qml.qnode(dev)
+def circuit():
+    ##################
+    # YOUR CODE HERE #
+    ##################
+
+    # IMPLEMENT THE CIRCUIT IN THE PICTURE AND MEASURE PAULI Y:
+    qml.RX(np.pi/4,wires=0)
+    qml.Hadamard(wires=0)
+    qml.PauliZ(wires=0)
+
+    return qml.expval(qml.PauliY(wires=0))
+
+
+print(circuit())
